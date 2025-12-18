00c9b113cd3def5898ddd2d103d60df61359cb7c
Eliot Robson <eliot.robson24@gmail.com>
2025-12-17 22:43:42 -0500

    Fix versioning

--- src/tldm/__init__.py
+++ src/tldm/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import version
+
 from ._monitor import TMonitor as TMonitor
 from .aliases import tenumerate as tenumerate
 from .aliases import tmap as tmap
@@ -5,3 +7,5 @@ from .aliases import tproduct as tproduct
 from .aliases import trange as trange
 from .aliases import tzip as tzip
 from .std import tldm as tldm
+
+__version__ = version("tldm")
